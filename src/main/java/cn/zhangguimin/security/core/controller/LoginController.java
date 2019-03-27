4173ed5a18128d0a83e9f11f7c14700c05155103
zgm <guimmm@126.com>
2019-03-27 23:17:35 +0800

    Mac 2019-03-27

--- src/main/java/cn/zhangguimin/security/core/controller/LoginController.java
+++ src/main/java/cn/zhangguimin/security/core/controller/LoginController.java
@@ -1,7 +1,10 @@
 package cn.zhangguimin.security.core.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Mr. Zhang
@@ -20,5 +23,17 @@ public class LoginController {
     @GetMapping(value = "/")
     public String index(){
         return "index";
+    }
+
+    /**
+     * 可以直接  getCurrentUser(Authentication  authentication) 直接return authentication，这样会返回很多没必要的数据
+     * 			可以使用 @AuthenticationPrincipal UserDetails user。返回的只是UserDetails信息
+     * @param user
+     * @return
+     */
+    @GetMapping("/me")
+    @ResponseBody
+    public Object getCurrentUser(@AuthenticationPrincipal UserDetails user) {
+        return user;
     }
 }
